6e7356299d4bc9b5ca99b216d6cfd19c49dd663a
Madi <72963376+Madi-S@users.noreply.github.com>
2021-02-26 18:14:46 +0600

    preliminary configuration

---
+++ app/config.py
@@ -0,0 +1,28 @@
+from datetime import timedelta
+from sys import platform
+
+
+class Config(object):
+    DEBUG = True
+
+    SECRET_KEY = '6554410059521c8b5426eba21e54a7a193aaf30e67645e8c24a2f6eb8779790711a4ca0b9d62ef43e79caddf1e5a123808a4ea370f69c59cd64003e1c9ac8659'
+
+    SQLALCHEMY_TRACK_MODIFICATIONS = False
+    
+    if platform.startswith('w'):
+        # For Windows
+        SQLALCHEMY_DATABASE_URI = r'sqlite:///C:\Users\khova\Desktop\Python\Code\Lead Generation\Lead-Generation\app\db\lg.db'
+
+    else:
+        # For Ubuntu
+        SQLALCHEMY_DATABASE_URI = 'mysql+mysqlconnector://root:1234@localhost/lg'
+
+
+    SESSION_TYPE = 'redis' # redis, memcached, filesystem or mongodb
+    PERMANENT_SESSION_LIFETIME = timedelta(days=90)
+
+    RECAPTCHA_SITE_KEY = '6LfwKVwaAAAAAH_XIbUUxY3Z17GUvOWO9niVhQZm'
+    RECAPTCHA_SECRET_KEY = '6LfwKVwaAAAAAC5vUdbssnDBUOJOytC2VBYY-TLl'
+    # RECAPTCHA_THEME = 'dark'
+    # RECAPTCHA_TYPE = 'audio'
+    # RECAPTCHA_SIZE = 'compact'
