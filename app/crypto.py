d273e084c48cc5e794137ffcaf68865598b27df6
Madi <72963376+Madi-S@users.noreply.github.com>
2021-02-26 18:14:53 +0600

    encrypt pwd

---
+++ app/crypto.py
@@ -0,0 +1,20 @@
+import hashlib
+import uuid
+
+
+salt = '405e45c08f7f4fa798e323b59cab69e4'
+
+
+def encrypt(password):
+    return hashlib.sha512(password.encode('utf-8') + salt.encode('utf-8')).hexdigest()
+
+
+if __name__ == '__main__':
+    pwd = 'qwerty123asdf'
+    print('Password: {pwd}')
+
+    for _ in range(10):
+        encrypted = encrypt(pwd)
+        print('---------------------')
+        print(f'Ecnrypted: {encrypted}, {len(encrypted)}')
+        
