c68e2599612cd9cf696c06701ffc0783ba37a6a5
Madi <72963376+Madi-S@users.noreply.github.com>
2021-02-27 20:44:20 +0600

    recaptcha keys renamed

--- app/config.py
+++ app/config.py
@@ -5,6 +5,8 @@ from sys import platform
 class Config(object):
     DEBUG = True
 
+    WTF_CSRF_SECRET_KEY = 'a random string'
+    
     SECRET_KEY = '6554410059521c8b5426eba21e54a7a193aaf30e67645e8c24a2f6eb8779790711a4ca0b9d62ef43e79caddf1e5a123808a4ea370f69c59cd64003e1c9ac8659'
 
     SQLALCHEMY_TRACK_MODIFICATIONS = False
@@ -21,8 +23,8 @@ class Config(object):
     SESSION_TYPE = 'redis' # redis, memcached, filesystem or mongodb
     PERMANENT_SESSION_LIFETIME = timedelta(days=90)
 
-    RECAPTCHA_SITE_KEY = '6LeENmoaAAAAAH1dMElRkLRGE2UiSP-CK831656U'
-    RECAPTCHA_SECRET_KEY = '6LeENmoaAAAAANfgmGgA9Of_IPlJWtppMLMoMoV4'
+    RECAPTCHA_PUBLIC_KEY = '6LeENmoaAAAAAH1dMElRkLRGE2UiSP-CK831656U'
+    RECAPTCHA_PRIVATE_KEY = '6LeENmoaAAAAANfgmGgA9Of_IPlJWtppMLMoMoV4'
     # RECAPTCHA_THEME = 'dark'
     # RECAPTCHA_TYPE = 'audio'
     # RECAPTCHA_SIZE = 'compact'
