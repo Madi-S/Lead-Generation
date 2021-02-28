54b8af7517efe0a476cacdbfb0e18e3894057b01
Madi <72963376+Madi-S@users.noreply.github.com>
2021-02-28 16:32:33 +0600

    user registration logic

--- app/views.py
+++ app/views.py
@@ -18,7 +18,7 @@ bad_user_agents = [
 def login_required(f):
     @wraps(f)
     def inner(*args, **kwargs):
-        if 'user_id' in session and User.get(session.get('user_id')).first():
+        if 'user_id' in session and User.query.get(session.get('user_id')):
             return f(*args, **kwargs)
 
         return redirect(url_for('register'))
@@ -39,9 +39,9 @@ def subscription_required(f):
 
 
 @app.before_request
-def check_user_agent():    
+def check_user_agent():
     user_agent = request.headers.get('User-Agent')
-    
+
     if not user_agent:
         return '<h1>Hi Bot! Check your cookies</h1><h2>If you are seeing this and you are a human, enable JavaScript and refresh your cookeis :D</h2>'
 
@@ -59,44 +59,59 @@ def main():
 @app.route('/register', methods=['GET', 'POST'])
 def register():
     form = UserRegistrationForm()
+    print(f'Form errors: {form.errors}')
 
-    if request.method == 'GET':        
+    if request.method == 'GET':
         return render_template('register.html', form=form)
 
     else:
         if form.validate_on_submit():
-            first_name = form.first_name.data
-            last_name = form.last_name.data
-            username = form.username.data
-            password = form.password.data
+            user = User.create(
+                form.password.data,
+                username=form.username.data,
+                last_name=form.last_name.data,
+                first_name=form.first_name.data,
+            )
+            if user:
+                session['user_id'] = user.id
+                session['username'] = user.username
+                return redirect(url_for('main'))
 
-            user = User.create(password,
-                                first_name=first_name,
-                                last_name=last_name,
-                                username=username)
+        flash('Registration failed: this username is already taken or password do not match', 'danger')
+        return redirect(url_for('register'))
+
+
+@app.route('/login', methods=['GET', 'POST'])
+def login():
+    form = UserLoginForm()
+
+    if request.method == 'GET':
+        return render_template('login.html', form=form)
+
+    if form.validate_on_submit():        
+        user = User.validate_user(form.username.data, form.password.data)
+        if user:
             session['user_id'] = user.id
             session['username'] = user.username
             return redirect(url_for('main'))
-        else:
-            flash('Submit Recaptcha!', 'danger')
-            return redirect(url_for('register'))
 
-@app.route('/login')
-def login():
-    return render_template('login.html')
+    flash('Username or/and password do not match', 'warning')
+    return redirect(url_for('login'))
+
 
 @app.route('/logout')
 @login_required
 def logout():
     session.pop('user_id', None)
+    session.pop('username', None)
     return redirect(url_for('main'))
 
 
 @app.route('/profile')
 @login_required
 def profile():
-    user_id = session.get('user_id')
-    return f'Hi User #{user_id}'
+    username = session.get('username')
+    return f'Hi {username}!'
 
 
 @app.route('/generate')
