bc05fe4a500058c23441d550c96ebb56178eee6a
Madi-S <khovansky99@gmail.com>
2024-09-24 22:56:40 +0500

    feat: setup.py version updated manually :(

--- setup.py
+++ setup.py
@@ -4,7 +4,7 @@ from setuptools import setup, find_packages
 with open('README.md', 'r') as f:
     long_description = f.read()
 
-VERSION = '0.3.0'
+VERSION = '1.0.1'
 
 setup(
     name='py_lead_generation',
